0aea73376c5d315687e44cf58c58c6ded36cbdba
amisyurov.c <amisyurov.c@arlo.com>
2023-10-30 20:16:03 +0600

    Addition relay for turn on GSM module. Addition Siren

--- leak_sensor/leak_sensor.ino
+++ leak_sensor/leak_sensor.ino
@@ -5,23 +5,31 @@
 // I defined const char* ssid, password, bot_token, chat_id here
 #include "credentials.h"
 
-constexpr long kSensorSensitivity = 120;
-constexpr long kSleepDuration = 1 * 60 * 1e6 / 6; // sleep 10s in ms
-constexpr long kAlarmDelay = 1 * 60 * 1e6 / 6; // sleep 1 min in ms
-const String kAlarmMessage = "Leak Detected!";
+#define A0_ANALOG A0
+#define D1_SIREN  5
+#define D2_RELAY  4
+#define D5_RX     14
+#define D6_TX     12
 
-struct {
-  uint32_t is_sleep_gsm;
-} rtcData;
+const String kAlarmMessage            = "Leak Detected!";   // message for sending to telegram and sms
+constexpr int kTone                   = 200;                // value for siren tone
+constexpr long kUartSpeed             = 9600;               // speed for uart
+constexpr long kSensorSensitivity     = 120;                // setting sensor sensetivity
+constexpr long kTimeForWifiConnection = 7000;               // time for try to cinnect to Wifi
+constexpr long kMicroDelay            = 100;                // delay for analog sensor
+constexpr long kLittleDelay           = 500;                // delay 500 ms
+constexpr long kWakeUpGsmDelay        = 15000;              // delay for registering sim
+constexpr long kSendSmsDelay          = 5000;               // delay for registering sim
+constexpr long kSleepDuration         = 1 * 60 * 1e6 / 15;  // sleep 10s in ms
+constexpr long long kAlarmDelay       = 1 * 60 * 1e6 / 30;  // sleep 1 min in ms
 
 void setup() {
-  Serial.begin(9600);
-  
-  delay(3000);
 
-  // We have only one analog PIN for esp8266, thus I use hardcode value for it.
-  int analogValue = analogRead(A0);
-  delay(100);
+  Serial.begin(kUartSpeed);
+
+  int analogValue = analogRead(A0_ANALOG);
+
+  delay(kMicroDelay);
 
   Serial.println("Sensor data: " + analogValue);
 
@@ -31,15 +39,11 @@ void setup() {
     send_message_to_telegram(kSsid, kPassword, kChatId, kBotToken, kAlarmMessage);
     send_SMS(kPhoneNumber, kAlarmMessage);
 
-    delay(5000);
-    // sleep_gsm();
     ESP.deepSleep(kAlarmDelay);
   } else {
     Serial.println("Everything's fine.");
   }
-  // sleep_gsm();
   ESP.deepSleep(kSleepDuration);
-
 }
 
 void loop() { }
@@ -49,33 +53,47 @@ void loop() { }
 * Sending SMS
 */
 void send_SMS(String phone_number, String message){
-  SoftwareSerial sim800l(14, 12); // D5, D6 ----> RX, TX
-  sim800l.begin(9600);
-  saveData(1);
+  SoftwareSerial sim800l(D5_RX, D6_TX);     // gpio 14, 12 ----> RX, TX
 
+  delay(kWakeUpGsmDelay);                   // time for ware up gsm module
+  
+  sim800l.begin(kUartSpeed);
+
+  pinMode(D1_SIREN, OUTPUT);                // siren turn on
+  tone(D1_SIREN, kTone);                    // select tone for siren
+  
+  delay(kLittleDelay);
   sim800l.println("AT");
-  delay(1000);
+  delay(kLittleDelay);
+  
+  // TODO validate answers from GSM module
   // String response = readResponse(sim800l);
   // if (response.indexOf("OK") == -1) {
   //     Serial.println("Error: No response for AT command");
-  //     sim800l.println("AT+CSCLK=1");
-  //     updateSerial(sim800l);
-  //     saveData(0);
   //     return;
   // }
 
-  sim800l.println("AT+CMGF=1"); // Configuring TEXT mode
+  sim800l.println("AT+CMGF=1");             // configuring TEXT mode
   sim800l.println("AT+CMGS=\"" + phone_number + "\"");
   updateSerial(sim800l);
   sim800l.print(message);
   updateSerial(sim800l);
   Serial.println("SMS Sent");
   sim800l.write(26);
-  // sim800l.println("AT+CSCLK=1");
-  // updateSerial(sim800l);
-  saveData(0);
+  updateSerial(sim800l);
+  // TODO recieve answer from gsm instead this delay.
+  delay(kSendSmsDelay);
+  digitalWrite(D2_RELAY, HIGH);             // gsm tern off
+  pinMode(D2_RELAY, OUTPUT);                // init pin for relay
+  digitalWrite(D2_RELAY, LOW);              // activate relay, powering gsm module
+  delay(kSendSmsDelay);                     // delay for siren
+  noTone(D1_SIREN);                         // siren turn off
 }
 
+
+/*
+* template funcs
+*
 void sleep_gsm() {
   if (ESP.rtcUserMemoryRead(0, (uint32_t*) &rtcData, sizeof(rtcData))) {
     uint32_t value = rtcData.is_sleep_gsm;
@@ -83,7 +101,7 @@ void sleep_gsm() {
     Serial.println(value);
     if(0 != value) {
       SoftwareSerial sim800l(14, 12); // D5, D6 ----> RX, TX
-      sim800l.begin(9600);
+      sim800l.begin(kUartSpeed);
       sim800l.println("AT+CSCLK=1");
       updateSerial(sim800l);
       saveData(0);
@@ -107,6 +125,7 @@ String readResponse(SoftwareSerial &sim800l) {
     }
     return response;
 }
+*/
 
 /*
 * Working with Wifi
@@ -116,7 +135,6 @@ void send_message_to_telegram(const char* ssid, const char* password, const char
 
   WiFiClientSecure wifiClient;
   UniversalTelegramBot bot(bot_token, wifiClient);
-
 
   if(0 == connect_wifi(ssid, password, wifiClient)) {
     if(0 == send_to_bot(bot, chat_id, message)) {
@@ -136,12 +154,12 @@ int connect_wifi(const char* ssid, const char* password, WiFiClientSecure &wifiC
   WiFi.begin(ssid, password);
 
   long lastcmd = millis();
-  // Trying to connect to wifi
+  // Trying wifi connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(kLittleDelay);
     Serial.print(".");
     
-    if(millis() - lastcmd > 7000) {
+    if(millis() - lastcmd > kTimeForWifiConnection) {
       lastcmd = millis();
       Serial.println("\nWiFi can not be connected");
       return 1;
@@ -169,6 +187,9 @@ int send_to_bot(UniversalTelegramBot &bot, const char* chat_id, String message) 
 * Logging
 */
 
+/*
+* template func
+*
 void test_module(SoftwareSerial &sim800l)
 {
   Serial.println(sim800l);
@@ -182,10 +203,11 @@ void test_module(SoftwareSerial &sim800l)
   updateSerial(sim800l);
   Serial.println();
 }
+*/
 
 void updateSerial(SoftwareSerial &sim800l)
 {
-  delay(500);
+  delay(kLittleDelay);
   while (Serial.available())
   {
     sim800l.write(Serial.read());//Forward what Serial received to Software Serial Port
@@ -200,5 +222,4 @@ void updateSerial(SoftwareSerial &sim800l)
 
 // AT инициализации
 // AT+CMGF=1 переход к смс
-// ATE0 хз что
 // AT+CMGS="+77051796703" указывам номер для смс
